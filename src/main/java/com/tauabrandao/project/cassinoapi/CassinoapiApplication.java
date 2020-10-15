1e9e99d3b2194410db8a7184eca02500f4d36177
Tauã Brandão <brandao.taua@gmail.com>
2020-10-14 21:46:43 -0300

    First Commit - Project started

---
+++ src/main/java/com/tauabrandao/project/cassinoapi/CassinoapiApplication.java
@@ -0,0 +1,13 @@
+package com.tauabrandao.project.cassinoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassinoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassinoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tauabrandao/project/cassinoapi/CassinoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tauabrandao.project.cassinoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassinoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
