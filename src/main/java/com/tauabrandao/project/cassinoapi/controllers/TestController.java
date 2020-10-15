01c59bb9868492c947b1ea5c5fbbed771bd4b269
Tauã Brandão <brandao.taua@gmail.com>
2020-10-14 21:54:17 -0300

    temporary removal of the spring security dependency. Test Controller working correctly.

---
+++ src/main/java/com/tauabrandao/project/cassinoapi/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.tauabrandao.project.cassinoapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping("/test")
+	public String testController() {
+		return "Working properly";
+	}
+	
+}
