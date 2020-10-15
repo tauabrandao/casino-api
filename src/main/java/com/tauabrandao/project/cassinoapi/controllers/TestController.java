274e9a624972a9aefcc1387cef7588af1d9e87d7
Tauã Brandão <brandao.taua@gmail.com>
2020-10-14 22:19:11 -0300

    Swagger added

--- src/main/java/com/tauabrandao/project/cassinoapi/controllers/TestController.java
+++ src/main/java/com/tauabrandao/project/cassinoapi/controllers/TestController.java
@@ -1,12 +1,17 @@
 package com.tauabrandao.project.cassinoapi.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value = "Test Controller")
 public class TestController {
 
-	@RequestMapping("/test")
+	@GetMapping("/test")
+	@ApiOperation(value = "End point test")
 	public String testController() {
 		return "Working properly";
 	}
