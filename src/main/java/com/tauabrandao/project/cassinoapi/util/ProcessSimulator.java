1d37abd2242dd849529b3bd7dbcb8704ee81af78
Tauã Brandão <brandao.taua@gmail.com>
2020-10-18 10:52:06 -0300

    Process Simulator Adjustments

--- src/main/java/com/tauabrandao/project/cassinoapi/util/ProcessSimulator.java
+++ src/main/java/com/tauabrandao/project/cassinoapi/util/ProcessSimulator.java
@@ -1,8 +1,6 @@
 package com.tauabrandao.project.cassinoapi.util;
 
-import java.security.SecureRandom;
-import java.time.LocalDate;
-import java.util.Date;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -11,37 +9,31 @@ import com.tauabrandao.project.cassinoapi.model.Account;
 import com.tauabrandao.project.cassinoapi.model.Cassino;
 import com.tauabrandao.project.cassinoapi.model.Game;
 import com.tauabrandao.project.cassinoapi.model.Player;
-import com.tauabrandao.project.cassinoapi.model.Session;
 import com.tauabrandao.project.cassinoapi.model.SessionIdentifier;
-import com.tauabrandao.project.cassinoapi.service.SessionIdentifierService;
+import com.tauabrandao.project.cassinoapi.service.CassinoService;
 
 @Component
 public class ProcessSimulator {
-	
+
 	@Autowired
-	SessionIdentifierService sessionIdentifierService;
+	private CassinoService cassinoService;
 
-	public SessionIdentifier generateIdentifier() {
-		SessionIdentifier sessionIdentifier = SessionIdentifier.builder().sessionId(generateSession().getId()).build();
-		sessionIdentifierService.save(sessionIdentifier);
-		return sessionIdentifier;
+	public SessionIdentifier generateIdentifier(String gameId) {
+		return SessionIdentifier.builder().gameId(gameId + "_U").sessionId(Long.toString(generateId()) + "_P").build();
 	}
 
-//	public Game generateGame() {
-//		Player player = Player.builder().id(playerId).build();
-//		return Game.builder().id(randomPositiveLong()).active(false).identifier(generateIdentifier()).player(player).build();
-//	}
-	
-	public Account generateAccount() {
-		return Account.builder().id(generateId()).active(true).balance(1000.).build();
+	public Game generateGame(Optional<Player> player, Cassino cassino) {
+		return Game.builder().id(generateId()).active(false).player(player.get()).cassino(cassino).build();
 	}
 
-	private Cassino generateCassino() {
-		return Cassino.builder().id(generateId()).name("Cassino name simulated").build();
+	public Account generateAccount(Player player) {
+		return Account.builder().id(generateId()).active(true).balance(1000.).currency("USD").player(player).build();
 	}
 
-	private Session generateSession() {
-		return Session.builder().id(generateId()).build();
+	public Cassino generateCassino() {
+		String cassinoName = "Simulated Cassino Name";
+		Optional<Cassino> cassino = cassinoService.findByName(cassinoName);
+		return cassino.isPresent() ? cassino.get() : Cassino.builder().name(cassinoName).build();
 	}
 
 	private long generateId() {
